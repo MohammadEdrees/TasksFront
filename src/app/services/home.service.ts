3149c79caeb8bcb0338f9d10e3a96d48800565e5
medoenoch <medoenoch@gmail.com>
2022-06-18 11:06:41 +0200

    tasks

--- src/app/services/home.service.ts
+++ src/app/services/home.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class HomeService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getTasks(){
+    return this.http.get<any>("").subscribe((data)=>{
+      console.log(data);
+    });
+
+  }
+  
 }
