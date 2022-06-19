fa07b109b12eff8e4563b4e0c3df624ffc0da14a
medoenoch <medoenoch@gmail.com>
2022-06-19 21:55:48 +0200

    some work

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { taskModel } from '../Models/taskModel';
+import { userModel } from '../Models/userModel';
+import { HomeService } from '../services/home.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
+  tasksList!: taskModel[];
+  members !:userModel[];
+  
+  constructor(private home:HomeService) { }
+  
   ngOnInit(): void {
+    this.getTasks();
+    this.getMembers();
   }
+   
+  getTasks()
+  {
+     this.home.getTasksApi().subscribe(data=>{
+      console.warn(data);
+     });
+  }
+   
+  getMembers()
+  {
+   this.home.getMembersApi().subscribe((data:userModel[])=>{
+    console.warn(data);
+    data.forEach(user=>{
+
+    })
+  });
+   console.log(this.members);
+   }
 
 }
